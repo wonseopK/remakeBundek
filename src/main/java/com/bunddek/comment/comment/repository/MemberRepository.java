ca42675cb7b1f1d1ab8417c2fa6f74dbdff48cdd
josh <josh@myfair.co>
2022-03-07 20:01:45 +0900

    master

--- src/main/java/com/bunddek/comment/comment/repository/MemberRepository.java
+++ src/main/java/com/bunddek/comment/comment/repository/MemberRepository.java
@@ -3,5 +3,6 @@ package com.bunddek.comment.comment.repository;
 import com.bunddek.comment.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//master
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
