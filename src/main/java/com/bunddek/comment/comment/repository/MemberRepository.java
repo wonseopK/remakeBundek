d711a9f0998f97b5c09b783d582675aaf3dbf99e
josh <josh@myfair.co>
2022-03-14 09:40:32 +0900

    refactor: yml h2 url 변경 / thymeleaf 속성 추가

--- src/main/java/com/bunddek/comment/comment/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bunddek.comment.comment.repository;
-
-import com.bunddek.comment.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
